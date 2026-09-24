e1fb98638091652c65a05dc6f327cc3926717ca5
Shreya Singh <shreyaaa.1626@gmail.com>
2026-09-24 16:36:32 +0530

    Add Leap Year Solution

---
+++ PS-Sprint-1/LeapYear.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class LeapYear{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the year: ");
+        int n = sc.nextInt();
+        //Check if Leap Year
+        if(n%4==0 && n%100!=0 || (n%400==0)){
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+        sc.close();
+    }
+}

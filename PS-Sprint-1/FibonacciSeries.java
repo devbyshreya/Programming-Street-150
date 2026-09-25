6a98be31152a638624211bd1845afbde6be4b117
Shreya Singh <shreyaaa.1626@gmail.com>
2026-09-25 19:24:13 +0530

    Update Leap Year solution

---
+++ PS-Sprint-1/FibonacciSeries.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class FibonacciSeries{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int limit = sc.nextInt();
+    }
+}
--- PS-Sprint-1/LeapYear.java
+++ PS-Sprint-1/LeapYear.java
@@ -3,9 +3,9 @@ public class LeapYear{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the year: ");
-        int n = sc.nextInt();
+        int year = sc.nextInt();
         //Check if Leap Year
-        if(n%4==0 && n%100!=0 || (n%400==0)){
+        if(year % 4 == 0 && year % 100 != 0 || year % 400 ==0 ) {
             System.out.println("Leap Year");
         } else {
             System.out.println("Not a Leap Year");

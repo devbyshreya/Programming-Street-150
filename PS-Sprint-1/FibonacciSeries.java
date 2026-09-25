46899b3dfeed679fce05d79478f01ab2857ca2c5
Shreya Singh <shreyaaa.1626@gmail.com>
2026-09-25 19:29:59 +0530

    AddFibonacci Series Solution

--- PS-Sprint-1/FibonacciSeries.java
+++ PS-Sprint-1/FibonacciSeries.java
@@ -2,7 +2,18 @@ import java.util.*;
 public class FibonacciSeries{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Limit = ");
         int limit = sc.nextInt();
+        int a = 0;
+        int b = 1;
+
+        while(a <= limit){
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        sc.close();
     }
 }
